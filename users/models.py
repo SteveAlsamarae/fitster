76324f7e77a98ac7f3a8955c353ed434e1800e07
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-01 02:19:20 +0600

    Add delivery-address model for users

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 from django_resized import ResizedImageField
+import uuid
 
 
 PHONE_NUMBER_VALIDATOR = RegexValidator(r"^[0-9]{11}$", "Enter a valid phone number.")
@@ -12,6 +14,8 @@ NAME_VALIDATOR = RegexValidator(
 
 
 class UserProfile(models.Model):
+    """Registered user's profile"""
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     avatar = ResizedImageField(
         size=[300, 300],
@@ -26,6 +30,7 @@ class UserProfile(models.Model):
     )
     name = models.CharField(max_length=100, validators=[NAME_VALIDATOR])
     phone = models.CharField(max_length=11, validators=[PHONE_NUMBER_VALIDATOR])
+    email = models.EmailField(_("email address"), unique=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
@@ -35,3 +40,29 @@ class UserProfile(models.Model):
         self.avatar.delete()
 
         return super(UserProfile, self).delete(*args, **kwargs)
+
+
+class DeliveryAddress(models.Model):
+    """
+    Customer address where the order will be delivered
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(
+        UserProfile, verbose_name=_("Customer"), on_delete=models.CASCADE
+    )
+    name = models.CharField(_("Full Name"), max_length=150)
+    phone = models.CharField(_("Contact Number"), max_length=50)
+    city = models.CharField(_("City/State"), max_length=150)
+    postcode = models.CharField(_("Postcode"), max_length=50)
+    area = models.CharField(_("Area"), max_length=255)
+    address = models.CharField(_("Address"), max_length=255, blank=True)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
+
+    def __str__(self):
+        return f"{self.name}'s delivery address."
