10f5a11465b940f76a8c67a4257e4f8d22aed3bb
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-03 00:40:46 +0600

    Add delivery address form for customer

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from .models import UserProfile
+from .models import UserProfile, DeliveryAddress
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -11,6 +11,18 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class UserProfileUpdateForm(forms.ModelForm):
+    """Fronend form for updating user profile"""
     class Meta:
         model = UserProfile
         fields = ["avatar", "name", "phone"]
+
+
+class DeliveryAddressForm(forms.ModelForm):
+    """Customer's delivery address form"""
+
+    class Meta:
+        model = DeliveryAddress
+        fields = ["name", "phone", "city", "postcode", "area", "address"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
