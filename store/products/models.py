e8212e25d96f0e67009aa450db607afa85e6ecd9
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-08 00:39:13 +0600

    Add product and product-category model with MPTT

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,129 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class ProductCategory(MPTTModel):
+    """
+    Product category model implimented with MPTT.
+    """
+
+    name = models.CharField(
+        verbose_name=_("Category Name"),
+        help_text=_("Required and unique"),
+        max_length=255,
+        unique=True,
+    )
+    slug = models.SlugField(verbose_name=_("Slug for url"), max_length=250, unique=True)
+    parent = TreeForeignKey(
+        "self",
+        verbose_name=_("Parent Category"),
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="subcategories",
+    )
+    is_active = models.BooleanField(default=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name, allow_unicode=True)
+
+        super(ProductCategory, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Product model with product necessary fields.
+    """
+
+    categories = models.ForeignKey(
+        ProductCategory, verbose_name=_("Product Categories"), on_delete=models.RESTRICT
+    )
+    title = models.CharField(
+        verbose_name=_("Product Title"),
+        help_text=_("Required"),
+        max_length=200,
+    )
+    description = models.TextField(
+        verbose_name=_("Product Description"), help_text=_("Optional"), blank=True
+    )
+    slug = models.SlugField(max_length=300, unique=True)
+    regular_price = models.DecimalField(
+        verbose_name=_("Regular price"),
+        max_digits=6,
+        decimal_places=2,
+    )
+    discount_price = models.DecimalField(
+        verbose_name=_("Discount price"),
+        max_digits=6,
+        decimal_places=2,
+    )
+    stocks = models.IntegerField(verbose_name=_("Stocks"), default=0)
+    is_active = models.BooleanField(
+        verbose_name=_("Product visibility"),
+        help_text=_("Change product visibility"),
+        default=True,
+    )
+    created_at = models.DateTimeField(
+        _("Created at"), auto_now_add=True, editable=False
+    )
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    users_wishlist = models.ManyToManyField(User, related_name="wishlist", blank=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title, allow_unicode=True)
+        return super(Product, self).save(*args, **kwargs)
+
+    def get_product_images(self):
+        return self.product_images.all()
+
+    @property
+    def get_discount_percentage(self):
+        return (
+            f"{int(round((1 - (self.discount_price / self.regular_price)) * 100, 0))}%"
+        )
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    """
+    Images for a specific product.
+    """
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_images"
+    )
+    image = models.ImageField(
+        verbose_name=_("image"),
+        help_text=_("Upload a product image"),
+        upload_to="products/",
+        default="images/product_default.png",
+    )
+
+    is_feature = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _("Product Images")
