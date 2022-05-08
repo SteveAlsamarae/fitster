e4f10896e886dd35a93715e413488e27e707dfcf
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-09 01:53:04 +0600

    Add abstract views for cart app and map urls

---
+++ store/cart/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+
+def cart_summary_view(request) -> HttpRequest:
+    pass
+
+
+def add_to_cart_view(request) -> HttpRequest:
+    pass
+
+
+def remove_from_cart_view(request) -> HttpRequest:
+    pass
+
+
+def clear_cart_view(request) -> HttpRequest:
+    pass
+
+
+def update_cart_view(request) -> HttpRequest:
+    pass
