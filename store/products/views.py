4f347eeef4f60b8043dc6dceabe010e56c90ae37
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-16 00:54:14 +0600

    Add pagination for shop page

--- store/products/views.py
+++ store/products/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
+from utils import paginate
+
 from .models import Product, ProductCategory
 
 
@@ -10,7 +12,16 @@ def index(request):
 
 def store_view(request: HttpRequest) -> HttpResponse:
     products = Product.objects.prefetch_related("product_images").filter(is_active=True)
-    return render(request, "store/shop.html", {"products": products})
+    page_obj = paginate(request, products, 10)
+
+    return render(
+        request,
+        "store/shop.html",
+        {
+            "products": products,
+            "page_obj": page_obj,
+        },
+    )
 
 
 def product_detail_view(request: HttpRequest, slug: str = None) -> HttpResponse:
@@ -18,7 +29,9 @@ def product_detail_view(request: HttpRequest, slug: str = None) -> HttpResponse:
     return render(request, "products/product_details.html", {"product": product})
 
 
-def products_in_category_view(request: HttpRequest, category_slug: str = None) -> HttpResponse:
+def products_in_category_view(
+    request: HttpRequest, category_slug: str = None
+) -> HttpResponse:
     category = get_object_or_404(ProductCategory, slug=category_slug)
     products = Product.objects.filter(
         categories__in=ProductCategory.objects.get(name=category_slug).get_descendants(
