1141a62d10b8ea5858e691f45aba028d6edee4e6
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-17 16:05:24 +0600

    Fix cart app views
    - Fix cart_summary_view
    - Fix add_to_cart
    - Fix remove_from_cart_view

--- store/cart/views.py
+++ store/cart/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 
@@ -7,7 +8,7 @@ from .models import Cart, CartItem
 
 
 def cart_summary_view(request: HttpRequest) -> HttpResponse:
-    """A view to show cart summery.
+    """A view to show cart summary.
 
     Args:
         request (HttpRequest): The request object.
@@ -16,15 +17,15 @@ def cart_summary_view(request: HttpRequest) -> HttpResponse:
         HttpResponse: The response object.
     """
 
-    if request.user.cart:
+    try:
         cart = request.user.cart
-    else:
+    except ObjectDoesNotExist:
         cart = Cart.objects.create(user=request.user)
 
-    return render(request, "cart/cart_summary.html", {"cart": cart})
+    return render(request, "store/cart_summary.html", {"cart": cart})
 
 
-def add_to_cart(request: HttpRequest, product_id) -> HttpResponse:
+def add_to_cart(request: HttpRequest, product_id: str) -> HttpResponse:
     """A view to add a product to cart.
 
     Args:
@@ -35,18 +36,28 @@ def add_to_cart(request: HttpRequest, product_id) -> HttpResponse:
         HttpResponse: The response object.
     """
 
-    if request.user.cart:
-        cart = request.user.cart
-    else:
+    try:
+        if request.user.cart:
+            cart = request.user.cart
+    except ObjectDoesNotExist:
         cart = Cart.objects.create(user=request.user)
 
-    product = Product.objects.get(id=product_id)
-    cart_item = CartItem.objects.create(cart=cart, product=product)
-    cart_item.save()
+    if request.method == "POST":
+        product = Product.objects.get(id=product_id)
+        if product.stocks > 0:
+            try:
+                cart_item = CartItem.objects.get(cart=cart, product=product)
+            except ObjectDoesNotExist:
+                cart_item = CartItem.objects.create(cart=cart, product=product)
+
+            quantities = request.POST.get("quantities", 1)
+            cart_item.quantity += int(quantities)
+            cart_item.save()
+
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def remove_from_cart_view(request: HttpRequest) -> HttpResponse:
+def remove_from_cart_view(request: HttpRequest, product_id: str) -> HttpResponse:
     """A view to remove a product from cart.
 
     Args:
@@ -56,17 +67,16 @@ def remove_from_cart_view(request: HttpRequest) -> HttpResponse:
         HttpResponse: The response object.
     """
 
-    if request.method == "POST":
+    try:
         if request.user.cart:
             cart = request.user.cart
-        else:
-            cart = Cart.objects.create(user=request.user)
-        product_id = request.POST.get("product_id")
-        cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
+    except ObjectDoesNotExist:
+        cart = Cart.objects.create(user=request.user)
+
+    cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
+    if cart_item:
         cart_item.delete()
-        return redirect("cart:summery")
-    else:
-        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+    return redirect("cart:summary")
 
 
 def clear_cart_view(request: HttpRequest) -> HttpResponse:
@@ -85,7 +95,7 @@ def clear_cart_view(request: HttpRequest) -> HttpResponse:
         else:
             cart = Cart.objects.create(user=request.user)
         cart.cart_items.all().delete()
-        return redirect("cart:summery")
+        return redirect("cart:summary")
     else:
         return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
@@ -110,6 +120,6 @@ def update_cart_view(request: HttpRequest) -> HttpResponse:
         cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
         cart_item.quantity = quantity
         cart_item.save()
-        return redirect("cart:summery")
+        return redirect("cart:summary")
     else:
         return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
