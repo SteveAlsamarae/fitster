14f52760f4b1074bf02362eddf6e790409401290
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-18 15:46:35 +0600

    Fix and refactor product app urls

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,20 +1,16 @@
 from django.urls import path
 
-from .views import (
-    index,
-    store_view,
-    product_detail_view,
-    products_in_category_view,
-)
+from .views import (index, product_detail_view, products_in_category_view,
+                    store_view)
 
 app_name = "products"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("shop/", store_view, name="store"),
-    path("shop/products/<slug:slug>", product_detail_view, name="product_detail"),
+    path("index", index, name="index"),
+    path("", store_view, name="store"),
+    path("products/<slug:slug>", product_detail_view, name="product_detail"),
     path(
-        "shop/categories/<slug:category_slug>/",
+        "categories/<slug:category_slug>/",
         products_in_category_view,
         name="category_products",
     ),
