27e63677f910eba6c79e1e8c093f7f1d23324c64
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-18 15:39:03 +0600

    Refactor and fix cart app urls

--- store/cart/urls.py
+++ store/cart/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 
-from .views import (
-    cart_summary_view,
-    add_to_cart_view,
-    remove_from_cart_view,
-    clear_cart_view,
-    update_cart_view,
-)
-
+from .views import (add_to_cart, cart_summary_view, clear_cart_view,
+                    remove_from_cart_view, update_cart_view)
 
 app_name = "cart"
 
 urlpatterns = [
-    path("summery", cart_summary_view, name="summery"),
-    path("add/", add_to_cart_view, name="add_to_cart"),
-    path("remove/", remove_from_cart_view, name="remove_from_cart"),
+    path("summary", cart_summary_view, name="summary"),
+    path("add/<str:product_id>", add_to_cart, name="add_to_cart"),
+    path("remove/<str:product_id>", remove_from_cart_view, name="remove_from_cart"),
     path("clear/", clear_cart_view, name="clear_cart"),
     path("update/", update_cart_view, name="update_cart"),
 ]
