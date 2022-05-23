d7e57d55e60eea31c7d0da4fe8f5975b593de5f6
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-23 16:51:20 +0600

    Add urls for fitness subscription checkout views

--- classes/urls.py
+++ classes/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from .views import (
     class_details_view,
     class_list_view,
+    fitness_subscription_checkout,
     fitness_subscription_pricing_view,
     fitster_team_view,
+    subscription_success,
     trainer_details_view,
 )
 
@@ -16,4 +18,6 @@ urlpatterns = [
     path("fitness/team/", fitster_team_view, name="team"),
     path("fitness/team/<str:slug>/", trainer_details_view, name="trainer"),
     path("fitness/pricing/", fitness_subscription_pricing_view, name="pricing"),
+    path("fitness/pricing/checkout", fitness_subscription_checkout, name="subscription_checkout"),
+    path("fitness/pricing/checkout/success/", subscription_success, name="subscription_success"),
 ]
