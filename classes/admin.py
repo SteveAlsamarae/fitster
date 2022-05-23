ee6836f6ba0cdf94d2c77f52522e99a0d9657d2a
SteveAlsamarae <sarsaif@yahoo.com>
2022-05-23 16:44:25 +0600

    Register classes model to admin

---
+++ classes/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from .models import (
+    ClassSchedule,
+    FitnessClass,
+    FitnessSubscriptionPlan,
+    NutritionPlan,
+    Trainer,
+    TrainerSkill,
+)
+
+
+class ClassScheduleInline(admin.TabularInline):
+    model = ClassSchedule
+    extra = 2
+
+
+class TrainerSkillInline(admin.TabularInline):
+    model = TrainerSkill
+    extra = 2
+
+
+admin.site.register(
+    FitnessClass,
+    inlines=[ClassScheduleInline],
+    list_display=["name", "slug", "phone", "email"],
+    search_fields=["name"],
+)
+
+admin.site.register(
+    Trainer,
+    inlines=[TrainerSkillInline],
+    list_display=["name", "title"],
+    search_fields=["name"],
+)
+
+admin.site.register(FitnessSubscriptionPlan)
+admin.site.register(NutritionPlan)
