e1dbfacb223a6b99c66d5dc749ce9259f5dcdc55
SteveAlsamarae <sarsaif@yahoo.com>
2022-06-06 01:32:25 +0600

    Add deployment enviroment variables script

---
+++ utils/envs/env.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "Export the environment variables..."
+
+export EMAIL_HOST_USER=
+export EMAIL_HOST_PASSWORD=
+
+export STRIPE_LIVE_SECRET_KEY=sk_live_YOUR_STRIPE_LIVE_SECRET_KEY
+export DJSTRIPE_WEBHOOK_SECRET=whsec_xxx
+
+export STRIPE_TEST_SECRET_KEY=
+export STRIPE_PUBLIC_KEY=
+
+export STRIPE_LIVE_MODE=false
+
+export AWS_STORAGE_BUCKET_NAME=
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_S3_REGION_NAME=
